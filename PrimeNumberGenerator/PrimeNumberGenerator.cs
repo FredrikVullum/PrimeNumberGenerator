609655ce977deadea2b5a81bf51ab8d505c0482a
Fredrik Vullum <vullum.fredrik@gmail.com>
2023-02-27 16:54:51 +0100

    Add project files.

---
+++ PrimeNumberGenerator/PrimeNumberGenerator.cs
@@ -0,0 +1,39 @@
+﻿
+
+namespace NumberGenerators
+{
+    public class PrimeNumberGenerator
+    {
+        private static bool IsPrime(int n)
+        {
+            if (n <= 1)
+                return false;
+
+            for(int i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+            return true;
+        }
+        public static int Generate(int targetNumberOfPrimeNumbersFound)
+        {
+            if (targetNumberOfPrimeNumbersFound <= 0)
+                throw new ArgumentOutOfRangeException("Input must be greater than zero");
+
+            int numberOfPrimesFound = 0;
+            int number = 2;
+            while (true)
+            {
+                if (IsPrime(number))
+                    numberOfPrimesFound++;
+
+                if (numberOfPrimesFound == targetNumberOfPrimeNumbersFound)
+                    break;
+
+                number++;
+            }
+            return number;
+        }
+    }
+}
---
+++ PrimeNumberGenerator/Program.cs
@@ -0,0 +1,7 @@
+﻿
+using NumberGenerators;
+
+
+//Console.WriteLine($"The 10001th prime number is: {PrimeNumberGenerator.Generate(0)}");
+Console.WriteLine($"The 10001th prime number is: {PrimeNumberGenerator.Generate(10001)}");
+
